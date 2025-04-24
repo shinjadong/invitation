db9be55f095908c8e63ac49a6980f243b941cb0e
shinjadong <tlswkehd@gmail.com>
2025-04-24 23:44:19 +0900

    next-themes 타입 오류 수정

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,8 +2,11 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+// NextThemesProvider의 props 타입을 그대로 사용
+export function ThemeProvider({ 
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
